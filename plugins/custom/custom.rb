27cda4f8e798aa233fa0eb59e1071f17c67cbf83
Pathfinder Mush <57819133+pathfinder-mush@users.noreply.github.com>
2019-11-29 01:18:20 -0500

    Created help file for goals command.

--- plugins/custom/custom.rb
+++ plugins/custom/custom.rb
@@ -5,12 +5,12 @@ module AresMUSH
     def self.plugin_dir
       File.dirname(__FILE__)
     end
- 
+
     def self.shortcuts
       Global.read_config("custom", "shortcuts")
     end
- 
-    def self.get_cmd_handler(client, cmd, enactor)      
+
+    def self.get_cmd_handler(client, cmd, enactor)
       return nil
     end
   end
