950e783251fe5191be1b3c4b183485622b77dbe9
Pathfinder Mush <dudyer@iu.edu>
2019-12-24 11:45:41 -0500

    Adding new arg parser to engine.
    arg1_equals_optional_arg2_slash_optional_arg3

--- engine/aresmush/commands/arg_parser.rb
+++ engine/aresmush/commands/arg_parser.rb
@@ -1,11 +1,11 @@
-module AresMUSH  
+module AresMUSH
   class ArgParser
-      
+
     def self.parse(regex, args)
       match = regex.match(args)
       !match ? HashReader.new({}) : HashReader.new(match.names_hash)
     end
-    
+
     def self.arg1_slash_arg2
       /(?<arg1>[^\/]+)\/(?<arg2>.+)/
     end
@@ -13,15 +13,15 @@ module AresMUSH
     def self.arg1_equals_arg2
       /(?<arg1>[^\=]+)\=(?<arg2>.+)/
     end
-    
+
     def self.arg1_equals_optional_arg2
       /(?<arg1>[^\=]+)\=?(?<arg2>.+)?/
     end
-    
+
     def self.arg1_slash_optional_arg2
       /(?<arg1>[^\/]+)\/?(?<arg2>.+)?/
     end
-    
+
     def self.arg1_equals_arg2_slash_arg3
       /(?<arg1>[^\=]+)\=(?<arg2>[^\/]+)\/(?<arg3>.+)/
     end
@@ -29,10 +29,12 @@ module AresMUSH
     def self.arg1_slash_arg2_equals_arg3
       /(?<arg1>[^\/]+)\/(?<arg2>[^\/]+)\=(?<arg3>.+)/
     end
-    
+
     def self.arg1_equals_arg2_slash_optional_arg3
       /(?<arg1>[^\=]+)\=(?<arg2>[^\/]+)\/?(?<arg3>.+)?/
     end
-    
+
+    def self.arg1_equals_optional_arg2_slash_optional_arg3
+      /(?<arg1>[^\=]+)\=?(?<arg2>[^\/]+)?\/?(?<arg3>.+)?/
   end
 end
