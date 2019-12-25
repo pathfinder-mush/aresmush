d66de050be9c727f4c3c93e354c7b2a8fc3d510a
Pathfinder Mush <dudyer@iu.edu>
2019-12-24 23:23:56 -0500

    Dice Code Debugging
    Changes to the dice code, with comments to make it more legible. Even though I'm not sure that it serves that purpose.

--- engine/aresmush/commands/arg_parser.rb
+++ engine/aresmush/commands/arg_parser.rb
@@ -33,9 +33,5 @@ module AresMUSH
     def self.arg1_equals_arg2_slash_optional_arg3
       /(?<arg1>[^\=]+)\=(?<arg2>[^\/]+)\/?(?<arg3>.+)?/
     end
-
-    def self.arg1_equals_optional_arg2_slash_optional_arg3
-      /(?<arg1>[^\=]+)\=?(?<arg2>[^\/]+)?\/?(?<arg3>.+)?/
-    end
   end
 end
---
+++ plugins/custom/arg_parsers.rb
@@ -0,0 +1,9 @@
+module AresMUSH
+  class CustomArgParsers
+
+    def self.arg1_equals_optional_arg2_slash_optional_arg3
+      /(?<arg1>[^\(=|\/)]+)\=?(?<arg2>[^\/]+)?\/?(?<arg3>.+)?/
+    end
+
+  end
+end
