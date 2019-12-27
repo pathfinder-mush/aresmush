abb86d88f6ee7d584f1e1ee158879b25fa42d96f
Pathfinder Mush <dudyer@iu.edu>
2019-12-26 23:49:37 -0500

    Template Debugging

--- plugins/pathfinder/templates/dice_roller_template.rb
+++ plugins/pathfinder/templates/dice_roller_template.rb
@@ -4,7 +4,7 @@ module AresMUSH
 
       attr_accessor :dice, :roll_target, :dice_rolls, :total, :comment
 
-      def initialize(dice, roll_target, dice_rolls, total, comment)
+      def initialize(self.dice, self.roll_target, self.dice_rolls, self.total, self.comment)
         super File.dirname(__FILE__) + "/roll.erb"
       end
 
