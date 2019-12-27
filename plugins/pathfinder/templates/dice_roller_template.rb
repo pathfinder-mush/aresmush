0d00d9a997f8f6fb75cfd42c50a308af7d267c68
Pathfinder Mush <dudyer@iu.edu>
2019-12-27 00:36:09 -0500

    Dice Code Debugging

--- plugins/pathfinder/templates/dice_roller_template.rb
+++ plugins/pathfinder/templates/dice_roller_template.rb
@@ -2,14 +2,15 @@ module AresMUSH
   module Pathfinder
     class DiceRollerTemplate < ErbTemplateRenderer
 
-      attr_accessor :roll, :roll_target, :dice_rolls, :total, :roll_comment
+      attr_accessor :roll, :roll_target, :dice_rolls, :total, :roll_comment, :enactor_name
 
-      def initialize(roll, roll_target, dice_rolls, total, roll_comment)
+      def initialize(roll, roll_target, dice_rolls, total, roll_comment, enactor_name)
         @roll = roll
         @roll_target = roll_target
         @dice_rolls = dice_rolls
         @total = total
         @roll_comment = roll_comment
+        @enactor_name = enactor_name
         super File.dirname(__FILE__) + "/roll.erb"
       end
 
